7c275f91d62d7ecf0087728f763cc56ee37b5698
Guto <gutomourao05@gmail.com>
2024-01-22 21:42:38 -0300

    feat: finished home

---
+++ src/components/DemostrationCard/index.tsx
@@ -0,0 +1,18 @@
+
+interface Props {
+    image: string
+    title: string
+}
+
+const DemostrationCard: React.FC<Props> = ({ image, title }): JSX.Element => {
+    return (
+        <div className=" cursor-pointer w-[100px] h-[120px] rounded-t-full bg-orange-600 shadow-lg flex flex-col items-center justify-center gap-3">
+            <div className="w-16 h-16 bg-white rounded-full flex items-center justify-center">
+                <img className="border-8 rounded-full" src={image} alt="" />
+            </div>
+            <span className="text-white text-xs">{title}</span>
+        </div>
+    )
+}
+
+export { DemostrationCard }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,53 +1,61 @@
 import { IoCartOutline } from "react-icons/io5";
 import { IoIosLogIn } from "react-icons/io";
 import { FaAngleDown } from "react-icons/fa";
-const Home = () => {
+import { DemostrationCard } from "../../components/DemostrationCard";
+const Home: React.FC = (): JSX.Element => {
     return (
-        <main className="w-full h-screen">
-            <main className="grid grid-cols-3 h-screen">
-                <div className="col-span-2 relative">
-                    <header className="w-full h-16 flex items-center justify-around">
-                        <img className="w-10 mt-1" src="./src/assets/img_pizza-fotor-2024012118438.png" alt="logo"/>
+        <main className="grid grid-cols-3">
+            <div className="col-span-2 relative flex flex-col items-center ">
+                <header className="w-full h-16 flex items-center justify-around">
+                    <img className="w-10 mt-1" src="./src/assets/img_pizza-fotor-2024012118438.png" alt="logo" />
 
-                        <ul className="flex gap-14">
-                            <li>
-                                <a href="">Home</a>
-                            </li>
-                            
-                            <li>
-                                <a href="">Contato</a>
-                            </li>
+                    <ul className="flex gap-10">
+                        <li>
+                            <a href="">Home</a>
+                        </li>
 
-														<li className="flex gap-2 items-center">
-                                <a href="" >Menu</a>
-																<FaAngleDown size={15}/>
-                            </li>
-                        </ul>
-                    </header>
+                        <li>
+                            <a href="">Contato</a>
+                        </li>
 
-										<section className="flex w-full h-screen items-center -ml-20 flex-col justify-center">
-											<div className="-mt-40  w-1/2">
-												<h1 className="text-5xl font-bold">Peperoni</h1>
-												<h1 className="text-5xl font-bold"> Pizza</h1>
-												<p className="mt-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum, ut earum! Neque provident ad vero assumenda libero, soluta doloribus, voluptates repellendus maxime molestias nostrum. Doloribus dolorem itaque rem rerum eos.</p>
-											</div>
-										</section>
+                        <li className="flex gap-2 items-center">
+                            <a href="" >Menu</a>
+                            <FaAngleDown size={15} />
+                        </li>
+                    </ul>
+                </header>
 
-                    <img className="rounded-full w-96 absolute -right-48 top-1/4 border-white border-8" src="./src/assets/img_pizza-fotor-2024012118438.png" alt="" />
-                </div>
+                <section className="flex w-full items-center -ml-40 mt-80 flex-col justify-center">
+                    <div className="-mt-40  w-1/2">
+                        <h1 className="text-5xl font-bold">Peperoni</h1>
+                        <h1 className="text-5xl font-bold"> Pizza</h1>
+                        <p className="mt-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum, ut earum! Neque provident ad vero assumenda libero, soluta doloribus, voluptates repellendus maxime molestias nostrum. Doloribus dolorem itaque rem rerum eos.</p>
+                        <button className="mt-5 bg-orange-600 text-white px-5 py-2 rounded">Veja Menu</button>
+                    </div>
+                </section>
 
-                <div className="col-span-1 bg-orange-600 h-screen">
-                    <header className="w-full h-16 flex items-center justify-center gap-10">
-                        <button className="bg-white rounded-full flex justify-center items-center p-2 ">
-                            <IoCartOutline size={20} color="gray"/>
-                        </button>
-                        <button className="bg-white rounded p-1 px-2 flex gap-2 items-center">
-														<IoIosLogIn size={20} color="gray"/>
-														Login
-												</button>
-                    </header>
-                </div>
-            </main>
+                <img className="rounded-full w-96 absolute -right-48 top-1/4 border-white border-8" src="./src/assets/img_pizza-fotor-2024012118438.png" alt="" />
+
+                <footer className="mt-32">
+                    <div className="bg-white flex gap-8">
+                        <DemostrationCard image="./src/assets/img_pizza-fotor-2024012118438.png" title="Pizza Doce" />
+                        <DemostrationCard image="./src/assets/img_pizza-fotor-2024012118438.png" title="Pizza Doce" />
+                        <DemostrationCard image="./src/assets/img_pizza-fotor-2024012118438.png" title="Pizza Doce" />
+                    </div>
+                </footer>
+            </div>
+
+            <div className="col-span-1 bg-orange-600 h-screen">
+                <header className="w-full h-16 flex items-center justify-center gap-10">
+                    <button className="bg-white rounded-full flex justify-center items-center p-2 ">
+                        <IoCartOutline size={20} color="gray" />
+                    </button>
+                    <button className="bg-white rounded p-1 px-2 flex gap-2 items-center">
+                        <IoIosLogIn size={20} color="gray" />
+                        Login
+                    </button>
+                </header>
+            </div>
         </main>
     );
 }
